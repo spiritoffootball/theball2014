901d3bd10ae6fc9148de43b4187d096f090b32f1
Christian Wach <needle@haystack.co.uk>
2021-04-01 21:25:20 +0100

    Load child theme translations properly

--- includes/class-theme.php
+++ includes/class-theme.php
@@ -143,7 +143,7 @@ class SOF_The_Ball_2014_Theme {
 		 * Make theme available for translation.
 		 * Translations can be added to the /languages/ directory of the child theme.
 		 */
-		load_theme_textdomain(
+		load_child_theme_textdomain(
 			'theball2014',
 			get_stylesheet_directory() . '/languages'
 		);
